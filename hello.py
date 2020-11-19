74964d69d332a32cfba5302ae1fb7067590721a2
Alistair Chambers <38084528+Cambrian34@users.noreply.github.com>
2020-11-19 15:23:21 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,96 @@
+print ('Hello')
+print ('What is your name?') # ask for users name
+myName=input()
+print (' It is nice to meet you, ' + myName)
+print ('There are')
+print (len(myName))
+print ('^letters in your name')
+print ('What is your age?') # ask for their age
+myAge=input()
+print ('You will be ' + str(int(myAge) + 1) +' within a year')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
